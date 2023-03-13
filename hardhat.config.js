c1ca17e4b76ca8eb823217c973753650ec771565
darren.eth <bagguo23@gmail.com>
2023-03-13 15:10:12 +0800

    mv uniswapv2 to samples;add dependencies

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,4 @@
 require("@nomicfoundation/hardhat-toolbox");
-require("@nomiclabs/hardhat-waffle");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -12,7 +11,14 @@ const INFURA_API_KEY = "f0f35ed9aa9748ad8a88dd6cdcf590c7";
 const GOERLI_PRIVATE_KEY = "0be427806c130f73704b2427e81f41ba1eabded4c9c3f91a74d50d646f52a992";
 
 module.exports = {
-  solidity: "0.8.9",
+  solidity: {
+    compilers: [
+      { version: "0.4.18" },
+      { version: "0.5.16" },
+      { version: "0.6.6" },
+      { version: "0.8.9" }
+    ]
+  },
   networks: {
     goerli: {
       url: `https://goerli.infura.io/v3/${INFURA_API_KEY}`,
