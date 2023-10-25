513d789430e87703ccc2b7aca3052344ce99158d
darren.eth <bagguo23@gmail.com>
2023-10-25 14:29:50 +0800

    upgrade

--- contracts/S09_Dos/Dos.sol
+++ contracts/S09_Dos/Dos.sol
@@ -1,12 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
+/**
+ * @title 拒绝服务（Denial of Service, DoS）漏洞
+ * 在 Web2 中，拒绝服务攻击（DoS）是指通过向服务器发送大量垃圾信息或干扰信息的方式，
+ * 导致服务器无法向正常用户提供服务的现象。
+ * 而在 Web3，它指的是利用漏洞使得智能合约无法正常提供服务
+ * 
+ * 案例：NFT 项目 Akutar
+ * @author
+ * @notice
+ */
 // 有DoS漏洞的游戏，玩家们先存钱，游戏结束后，调用deposit退钱。
 contract DoSGame {
     bool public refundFinished;
     mapping(address => uint256) public balanceOf;
     address[] public players;
-    
+
     // 所有玩家存ETH到合约里
     function deposit() external payable {
         require(!refundFinished, "Game Over");
@@ -22,7 +32,7 @@ contract DoSGame {
         require(!refundFinished, "Game Over");
         uint256 pLength = players.length;
         // 通过循环给所有玩家退款
-        for(uint256 i; i < pLength; i++){
+        for (uint256 i; i < pLength; i++) {
             address player = players[i];
             uint256 refundETH = balanceOf[player];
             (bool success, ) = player.call{value: refundETH}("");
@@ -32,14 +42,14 @@ contract DoSGame {
         refundFinished = true;
     }
 
-    function balance() external view returns(uint256){
+    function balance() external view returns (uint256) {
         return address(this).balance;
     }
 }
 
 contract Attack {
     // 退款时进行DoS攻击
-    fallback() external payable{
+    fallback() external payable {
         revert("DoS Attack!");
     }
 
